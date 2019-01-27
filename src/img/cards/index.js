86546530cdfeba61b15188726770eec562224977
Pliavi <pliavi.rpgm@gmail.com>
2019-01-27 16:18:51 -0200

    renamed image and component files

--- src/img/cards/index.js
+++
@@ -1,9 +0,0 @@
-import Card0 from "./CardFront0.svg";
-import Card1 from "./CardFront1.svg";
-import Card2 from "./CardFront2.svg";
-
-export default [
-  { id: 0, image: Card0 },
-  { id: 1, image: Card1 },
-  { id: 2, image: Card2 }
-];
