4860976e6ca7de8118585cab4ab9cbe8feb6a1f2
Pliavi <pliavi.rpgm@gmail.com>
2019-01-28 13:48:03 -0200

    More cards! More cards!

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,12 +1,22 @@
 import AngularFront from "./Angular.svg";
 import ReactFront from "./React.svg";
 import VueFront from "./Vue.svg";
+import RiotFront from "./Riot.svg";
+import MarkoFront from "./Marko.svg";
+import BackboneFront from "./Backbone.svg";
+import KnockoutFront from "./Knockout.svg";
+import PolymerFront from "./Polymer.svg";
 
 export default function Deck() {
   const cards = [
     { id: 0, flip: 0, front: AngularFront },
     { id: 1, flip: 0, front: ReactFront },
-    { id: 2, flip: 0, front: VueFront }
+    { id: 2, flip: 0, front: VueFront },
+    { id: 3, flip: 0, front: RiotFront },
+    { id: 4, flip: 0, front: MarkoFront },
+    { id: 5, flip: 0, front: BackboneFront },
+    { id: 6, flip: 0, front: KnockoutFront },
+    { id: 7, flip: 0, front: PolymerFront }
   ];
 
   const clone = cards.map(card => ({ ...card }));
