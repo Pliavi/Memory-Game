3ac727ba51b64a1efb5062917f5ac0b8e3edd182
Pliavi <pliavi.rpgm@gmail.com>
2019-01-28 13:47:48 -0200

    creating a restart function after won

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from "react";
 import shuffledDeck from "../Deck/Deck";
 import Card, { FLIPPING, FLIPPED, UNFLIPPED } from "../Card/Card";
+import "./Board.css";
 
 export default function Board() {
   const [deck, setDeck] = useState(shuffledDeck());
   const [hand, setHand] = useState([]);
+  const [won, setWon] = useState(false);
+
+  function handleRestart() {
+    deck.forEach(card => {
+      window.setTimeout(() => {
+        card.flip = UNFLIPPED;
+        setDeck(deck);
+      }, 500);
+    });
+
+    window.setTimeout(() => {
+      setDeck(shuffledDeck());
+      setHand([]);
+      setWon(false);
+    }, 1000);
+  }
 
   function handleFlip(i) {
     const card = deck[i];
+
+    if (hand.length === 2) return;
+    if (card.flip === FLIPPING) return;
 
     if (card.flip !== FLIPPED || card.flip !== UNFLIPPED) {
       card.flip = FLIPPING;
@@ -22,30 +42,32 @@ export default function Board() {
   }
 
   useEffect(() => {
-    if (hand.length === 2) {
-      if (hand[1].flip === FLIPPED) {
-        if (hand[0].id !== hand[1].id) {
-          hand.forEach(card => (card.flip = UNFLIPPED));
-        }
-        setHand([]);
+    if (hand.length === 2 && hand[1].flip === FLIPPED) {
+      if (hand[0].id !== hand[1].id) {
+        hand.forEach(card => (card.flip = UNFLIPPED));
       }
+      setHand([]);
     }
   });
 
   useEffect(
     () => {
-      if (deck.every(card => card.flip)) {
-        console.log("You won!");
-      }
+      setWon(deck.every(card => card.flip === FLIPPED));
     },
     [hand]
   );
 
   return (
-    <div className="board">
-      {deck.map((card, i) => (
-        <Card {...card} key={i} onClick={() => handleFlip(i)} />
-      ))}
-    </div>
+    <>
+      <div className="board">
+        {deck.map((card, i) => (
+          <Card {...card} key={i} onClick={() => handleFlip(i)} />
+        ))}
+      </div>
+      <div className={"won " + (won ? "--show" : "")}>
+        <h1>Você venceu!</h1>
+        <button onClick={handleRestart}>Recomeçar</button>
+      </div>
+    </>
   );
 }
